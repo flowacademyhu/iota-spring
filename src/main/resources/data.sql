dfd9f289d76854ec248d13f76fc5ccbd8c4bed53
Robert Boros <robbie13.boros@gmail.com>
2021-01-12 08:49:29 +0100

    add db setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO test (id, name, data) VALUES (null, "some name", 200);
+INSERT INTO test (id, name, data) VALUES (null, "some name 2", 300);
+INSERT INTO test (id, name, data) VALUES (null, "some name 3", 400);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table if exists test;
+create table IF NOT EXISTS test(id int auto_increment primary key, name varchar(255), data int);
