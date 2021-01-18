753d3884acf25508920674e134c2450b30cee9c2
Robert Boros <robbie13.boros@gmail.com>
2021-01-18 09:25:05 +0100

    add random port choose to int test

--- src/test/java/hu/flowacademy/iotaspring/IotaSpringApplicationTests.java
+++ src/test/java/hu/flowacademy/iotaspring/IotaSpringApplicationTests.java
@@ -2,30 +2,29 @@ package hu.flowacademy.iotaspring;
 
 import hu.flowacademy.iotaspring.exchange.ExchangeData;
 import hu.flowacademy.iotaspring.exchange.ExchangeJpaRepository;
-import hu.flowacademy.iotaspring.exchange.ExchangeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class IotaSpringApplicationTests {
@@ -37,13 +36,13 @@ class IotaSpringApplicationTests {
 	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	void testSave() {
 		exchangeJpaRepository.save(ExchangeData.builder()
 				.id(UUID.randomUUID().toString())
 				.to("USD").from("HUF").result(BigDecimal.valueOf(29400)).amount(BigDecimal.valueOf(100))
 				.build());
-		List<ExchangeData> all = exchangeJpaRepository.findAll();
-		log.info("data is {}", all);
+
+		Assertions.assertEquals(1, exchangeJpaRepository.findAll().size());
 	}
 
 	@Test
